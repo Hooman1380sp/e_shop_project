4a8c1bbd1f101f17e8d359ed0dcd7f3646348b07
Wilson Weber <sanjabian.ho@gmail.com>
2023-06-08 12:58:49 +0330

    Reformatted Code, Changed Some Logics, Deleted Some Duplicate Codes

--- accounte_module/views.py
+++ accounte_module/views.py
@@ -1,197 +1,234 @@
 from django.shortcuts import redirect
-from django.contrib.auth import get_user_model# زمانی که یک user جنگو سفارشی درست میکنیم باید به این شکل مدل user را معرفی کنیم
+from django.contrib.auth import \
+    get_user_model  # زمانی که یک user جنگو سفارشی درست میکنیم باید به این شکل مدل user را معرفی کنیم
 from .permissions import PermissionEditUserProfile
 from .serializers import *
 from rest_framework.views import APIView
-from django.http import HttpRequest, Http404,HttpResponseNotFound
-from rest_framework_simplejwt.tokens import RefreshToken #برای درست کردن توکن jwt قبل از ریجیستر
+from django.http import HttpRequest, Http404, HttpResponseNotFound
+from rest_framework_simplejwt.tokens import RefreshToken  # برای درست کردن توکن jwt قبل از ریجیستر
 from rest_framework.response import Response
 from rest_framework import status
 from django.utils.crypto import get_random_string
 from django.views import View
 import smtplib
 from email.message import EmailMessage
-from rest_framework.throttling import AnonRateThrottle,UserRateThrottle
+from rest_framework.throttling import AnonRateThrottle, UserRateThrottle
 from rest_framework.permissions import IsAuthenticatedOrReadOnly
-from django.contrib.auth import login,logout
-#Create your views here.
+from django.contrib.auth import login, logout
+from .config import *
+
+# Create your views here.
 
 
 def get_token_for_user(user):
     refresh = RefreshToken.for_user(user)
     return {
-        'refresh' : str(refresh),
-        'access' : str(refresh.access_token),
+        'refresh': str(refresh),
+        'access': str(refresh.access_token),
     }
-User = get_user_model() # زمانی که یک user جنگو سفارشی درست میکنیم باید به این شکل مدل user را معرفی کنیم
+
+
+User = get_user_model()  # زمانی که یک user جنگو سفارشی درست میکنیم باید به این شکل مدل user را معرفی کنیم
+
 
 class UserRegisterView(APIView):
     serializer_class = UserRegisterSerializer
-    throttle_classes = [UserRateThrottle,AnonRateThrottle]
+    throttle_classes = [UserRateThrottle, AnonRateThrottle]
     """
     create a new user (user register page)
     
     """
-    def post(self,request:HttpRequest):
+
+    def post(self, request: HttpRequest):
         ser_date = UserRegisterSerializer(data=request.POST)
-        if ser_date.is_valid():
-            user_email = ser_date.validated_data.get('email')
-            user_password = ser_date.validated_data.get('password')
-            user : bool = User.objects.filter(email__iexact=user_email).exists()
-            if user == True:
-                return Response({'error message': 'ایمیل وارد شده تکراری می باشد'},status=status.HTTP_406_NOT_ACCEPTABLE)
-            else:
-                random_str=get_random_string(84)
-                new_user = User(email=user_email,username=user_email,is_active=False,email_active_code=random_str)
-                new_user.set_password(user_password)
-                # get_token_for_user(new_user)
-                new_user.save()
-                EMAIL_HOST_PASSWORD = 'qjvhrbqewwvqxmxp'
-                EMAIL_HOST = 'smtp.gmail.com'
-                EMAIL_HOST_USER = 'testingmyworksdjango@gmail.com'
-                EMAIL_PORT_SSL = 465
-                msg = EmailMessage()
-                msg['Subject'] = 'Activate account'
-                msg['Form'] = EMAIL_HOST_USER
-                msg['To'] = user_email
-                msg.set_content(f'http://localhost:8000/account/activate-account/{random_str}')
-                with smtplib.SMTP_SSL(EMAIL_HOST,EMAIL_PORT_SSL) as server:
-                    server.login(EMAIL_HOST_USER,EMAIL_HOST_PASSWORD)
-                    server.send_message(msg)
-                return Response(data=ser_date.data,status=status.HTTP_201_CREATED)
-        return Response(data=ser_date.errors,status=status.HTTP_406_NOT_ACCEPTABLE)
+        if not ser_date.is_valid():
+            return Response(data=ser_date.errors, status=status.HTTP_406_NOT_ACCEPTABLE)
+
+        user_email = ser_date.validated_data.get('email')
+        user_password = ser_date.validated_data.get('password')
+        user: bool = User.objects.filter(email__iexact=user_email).exists()
+        if user:
+            return Response({'error message': 'ایمیل وارد شده تکراری می باشد'}, status=status.HTTP_406_NOT_ACCEPTABLE)
+
+        random_str = get_random_string(84)
+        new_user = User(email=user_email, username=user_email, is_active=False, email_active_code=random_str)
+        new_user.set_password(user_password)
+        # get_token_for_user(new_user)
+        new_user.save()
+
+        msg = EmailMessage()
+        msg['Subject'] = 'Activate account'
+        msg['Form'] = EMAIL_HOST_USER
+        msg['To'] = user_email
+        msg.set_content(f'http://localhost:8000/account/activate-account/{random_str}')
+        with smtplib.SMTP_SSL(EMAIL_HOST, EMAIL_PORT_SSL) as server:
+            server.login(EMAIL_HOST_USER, EMAIL_HOST_PASSWORD)
+            server.send_message(msg)
+        return Response(data=ser_date.data, status=status.HTTP_201_CREATED)
+
 
 class ActivateAccountView(View):
-    def get(self,request,email_active_code):
-        user : User = User.objects.filter(email_active_code__iexact = email_active_code).first()
-        if user is not None:
-                user.is_active = True
-                print('activated')
-                user.email_active_code = get_random_string(84)
-                user.save()
-                return redirect('http://localhost:8000')
-        return HttpResponseNotFound('error')
+    def get(self, request, email_active_code):
+        user: User = User.objects.filter(email_active_code__iexact=email_active_code).first()
+        if user is None:
+            return HttpResponseNotFound('error')
+
+        user.is_active = True
+        print('activated')
+        user.email_active_code = get_random_string(84)
+        user.save()
+        return redirect('http://localhost:8000')
+
+
 
 class UserLoginView(APIView):
     serializer_class = UserLoginSerializer
-    throttle_classes = [UserRateThrottle,AnonRateThrottle]
+    throttle_classes = [UserRateThrottle, AnonRateThrottle]
     """
     page login view  
     """
-    def post(self,request):
+
+    def post(self, request):
         ser_data = UserLoginSerializer(data=request.POST)
-        if ser_data.is_valid():
-            user_email = ser_data.validated_data.get('email')
-            user_password = ser_data.validated_data.get('password')
-            user : User = User.objects.filter(email=user_email).first()
-            if user is not None:
-                if not user.is_active:
-                    return Response({'message' : 'اکانت شما فعال نشده است'},status=status.HTTP_406_NOT_ACCEPTABLE)
-                else:
-                    if user_email == user.email:
-                        check_password = user.check_password(user_password)
-                        if check_password:
-                            login(request,user)
-                            return Response(ser_data.data,status=status.HTTP_202_ACCEPTED)
-                        return Response({'message': 'کلمه عبور وارد شده اشتباه است'},status=status.HTTP_400_BAD_REQUEST)
-                    return Response({'message': 'ایمیل وارد شده اشتباه است'},status=status.HTTP_406_NOT_ACCEPTABLE)
-            return Response({'message' : 'کاربری با مشخصات شما یافت نشده است'},status=status.HTTP_406_NOT_ACCEPTABLE)
-        return Response(data=ser_data.errors,status=status.HTTP_400_BAD_REQUEST)
+        if not ser_data.is_valid():
+            return Response(data=ser_data.errors, status=status.HTTP_400_BAD_REQUEST)
+
+        user_email = ser_data.validated_data.get('email')
+        user_password = ser_data.validated_data.get('password')
+        user: User = User.objects.filter(email=user_email).first()
+        if user is  None:
+            return Response({'message': 'کاربری با مشخصات شما یافت نشده است'}, status=status.HTTP_406_NOT_ACCEPTABLE)
+
+        if not user.is_active:
+            return Response({'message': 'اکانت شما فعال نشده است'}, status=status.HTTP_406_NOT_ACCEPTABLE)
+
+        if user_email != user.email:
+            return Response({'message': 'ایمیل وارد شده اشتباه است'}, status=status.HTTP_406_NOT_ACCEPTABLE)
+
+        check_password = user.check_password(user_password)
+        if not check_password:
+            return Response({'message': 'کلمه عبور وارد شده اشتباه است'},
+                            status=status.HTTP_400_BAD_REQUEST)
+        login(request, user)
+        return Response(ser_data.data, status=status.HTTP_202_ACCEPTED)
+
+
+
+
+
 
 class UserLogoutView(View):
-    def get(self,request):
+    def get(self, request):
         logout(request)
         print('logout shod')
         return redirect('http://localhost:8000')
 
+
 class UserForgotPasswordView(APIView):
-    throttle_classes = [UserRateThrottle,AnonRateThrottle]
+    throttle_classes = [UserRateThrottle, AnonRateThrottle]
     serializer_class = UserForgotPasswordSerializer
     """
     page forget password
     """
-    def post(self,request:HttpRequest):
+
+    def post(self, request: HttpRequest):
         ser_data = UserForgotPasswordSerializer(data=request.POST)
-        if ser_data.is_valid():
-            user_email = ser_data.validated_data.get('email')
-            user = User.objects.filter(email__iexact=user_email).first()
-            if user is not None:
-                random_str = user.email_active_code
-                EMAIL_HOST_PASSWORD = 'qjvhrbqewwvqxmxp'
-                EMAIL_HOST = 'smtp.gmail.com'
-                EMAIL_HOST_USER = 'testingmyworksdjango@gmail.com'
-                EMAIL_PORT_SSL = 465
-                msg = EmailMessage()
-                msg['Subject'] = 'reset password account'
-                msg['Form'] = EMAIL_HOST_USER
-                msg['To'] = user_email
-                msg.set_content(f'http://localhost:8000/account/reset-pass/{random_str}')
-                with smtplib.SMTP_SSL(EMAIL_HOST, EMAIL_PORT_SSL) as server:
-                    server.login(EMAIL_HOST_USER, EMAIL_HOST_PASSWORD)
-                    server.send_message(msg)
-                    return Response(data=ser_data.data,status=status.HTTP_202_ACCEPTED)
-            return Response({'message': 'ایمیل وارد شده, قبلا ثبت نام نکرده است '},status=status.HTTP_406_NOT_ACCEPTABLE)
-        return Response({'message': 'در وارد کردن اطلاعات خود دقت کنید' },status=status.HTTP_400_BAD_REQUEST)
+        if not ser_data.is_valid():
+            return Response({'message': 'در وارد کردن اطلاعات خود دقت کنید'}, status=status.HTTP_400_BAD_REQUEST)
+
+
+        user_email = ser_data.validated_data.get('email')
+        user = User.objects.filter(email__iexact=user_email).first()
+        if user is None:
+            return Response({'message': 'ایمیل وارد شده, قبلا ثبت نام نکرده است '},
+                            status=status.HTTP_406_NOT_ACCEPTABLE)
+
+        random_str = user.email_active_code
+        msg = EmailMessage()
+        msg['Subject'] = 'reset password account'
+        msg['Form'] = EMAIL_HOST_USER
+        msg['To'] = user_email
+        msg.set_content(f'http://localhost:8000/account/reset-pass/{random_str}')
+        with smtplib.SMTP_SSL(EMAIL_HOST, EMAIL_PORT_SSL) as server:
+            server.login(EMAIL_HOST_USER, EMAIL_HOST_PASSWORD)
+            server.send_message(msg)
+            return Response(data=ser_data.data, status=status.HTTP_202_ACCEPTED)
+
+
+
 
 class UserResetPasswordView(APIView):
     serializer_class = UserResetPasswordSerializer
-    throttle_classes = [UserRateThrottle,AnonRateThrottle]
+    throttle_classes = [UserRateThrottle, AnonRateThrottle]
     """
     page reset password!
     """
-    def post(self,request:HttpRequest,active_code):
-        user : User = User.objects.filter(email_active_code__iexact=active_code).first()
-        if user is not None:
-            ser_data = UserResetPasswordSerializer(instance=user,data=request.POST)
-            if ser_data.is_valid():
-                # ser_data.save() # این روش غیر امنیتی هست
-                user_password = ser_data.validated_data.get('password')
-                user.set_password(user_password)
-                user.is_active = True
-                user.email_active_code = get_random_string(84)
-                user.save()
-                return Response(data=ser_data.data,status=status.HTTP_202_ACCEPTED)
-            return Response({'message': 'در وارد کردن اطلاعات خود دقت کنید'},status=status.HTTP_400_BAD_REQUEST)
-        return Response({'message': 'کاربری با همچنین ایمیلی ثبت نشده است'},status=status.HTTP_404_NOT_FOUND)
+
+    def post(self, request: HttpRequest, active_code):
+        user: User = User.objects.filter(email_active_code__iexact=active_code).first()
+        if user is None:
+            return Response({'message': 'کاربری با همچنین ایمیلی ثبت نشده است'}, status=status.HTTP_404_NOT_FOUND)
+
+        ser_data = UserResetPasswordSerializer(instance=user, data=request.POST)
+        if not ser_data.is_valid():
+            return Response({'message': 'در وارد کردن اطلاعات خود دقت کنید'}, status=status.HTTP_400_BAD_REQUEST)
+
+        # ser_data.save() # این روش غیر امنیتی هست
+        user_password = ser_data.validated_data.get('password')
+        user.set_password(user_password)
+        user.is_active = True
+        user.email_active_code = get_random_string(84)
+        user.save()
+        return Response(data=ser_data.data, status=status.HTTP_202_ACCEPTED)
+
+
     # def put(self,request:HttpRequest):
     #     ser_date = UserRegisterSerializers(data=request.POST,partial=True)
 
+
 class EditUserProfileView(APIView):
     serializer_class = EditUserProfileSerializer
-    throttle_classes = [AnonRateThrottle,UserRateThrottle]
-    permission_classes = [PermissionEditUserProfile,]
+    throttle_classes = [AnonRateThrottle, UserRateThrottle]
+    permission_classes = [PermissionEditUserProfile, ]
     """
     this page for edit profile
 
     """
-    def put(self,request: HttpRequest):
+
+    def put(self, request: HttpRequest):
         current_user: User = User.objects.filter(id=request.user.id).first()
         self.check_object_permissions(request, current_user)
-        ser_data = EditUserProfileSerializer(instance=current_user,data=request.POST,partial=True)
+        ser_data = EditUserProfileSerializer(instance=current_user, data=request.POST, partial=True)
         if ser_data.is_valid():
             ser_data.save()
-            return Response(data=ser_data.data,status=status.HTTP_206_PARTIAL_CONTENT)
-        return Response(ser_data.errors,status=status.HTTP_409_CONFLICT)
+            return Response(data=ser_data.data, status=status.HTTP_206_PARTIAL_CONTENT)
+        return Response(ser_data.errors, status=status.HTTP_409_CONFLICT)
+
 
 class ChangePasswordAccountView(APIView):
     serializer_class = ChangePasswordAccoutSerializer
-    throttle_classes = [AnonRateThrottle,UserRateThrottle]
+    throttle_classes = [AnonRateThrottle, UserRateThrottle]
     permission_classes = [PermissionEditUserProfile]
 
     """
     
     """
-    def post(self,request:HttpRequest):
-       user: User = User.objects.filter(id=request.user.id).first()
-       ser_data = ChangePasswordAccoutSerializer(data=request.POST)
-       if ser_data.is_valid():
-           current_password = ser_data.validated_data.get('current_password')
-           new_password = ser_data.validated_data.get('new_password')
-           if user.check_password(current_password):
-                print(current_password)
-                print(new_password)
-                user.set_password(new_password)
-                user.save()
-                return Response(data=ser_data.data,status=status.HTTP_202_ACCEPTED)
-           return Response({'message': 'کلمه عبور اشتباه میباشد'},status=status.HTTP_409_CONFLICT)
-       return Response({'message': 'تداخل در انجام عملیات'}, status=status.HTTP_409_CONFLICT)
+
+    def post(self, request: HttpRequest):
+        user: User = User.objects.filter(id=request.user.id).first()
+        ser_data = ChangePasswordAccoutSerializer(data=request.POST)
+        if not ser_data.is_valid():
+            return Response({'message': 'تداخل در انجام عملیات'}, status=status.HTTP_409_CONFLICT)
+
+        current_password = ser_data.validated_data.get('current_password')
+        new_password = ser_data.validated_data.get('new_password')
+        if not user.check_password(current_password):
+            return Response({'message': 'کلمه عبور اشتباه میباشد'}, status=status.HTTP_409_CONFLICT)
+
+        print(current_password)
+        print(new_password)
+        user.set_password(new_password)
+        user.save()
+        return Response(data=ser_data.data, status=status.HTTP_202_ACCEPTED)
+
+
--- cart_module/views.py
+++ cart_module/views.py
@@ -1,4 +1,7 @@
+import json
 import time
+
+import requests
 from django.contrib.auth.decorators import login_required
 from django.http import HttpRequest
 from django.shortcuts import redirect
@@ -7,68 +10,66 @@ from rest_framework.permissions import IsAuthenticatedOrReadOnly
 from rest_framework.response import Response
 from rest_framework.throttling import AnonRateThrottle, UserRateThrottle
 from rest_framework.views import APIView
-import requests
-import json
+
 from product_module.models import Product
+from .config import (
+    ZP_API_REQUEST,
+    ZP_API_VERIFY,
+    ZP_API_STARTPAY,
+    description,
+    CallbackURL,
+    MERCHANT
+)
 from .models import Cart, CartDetail
-from .serializers import AddProductToCartSerializer,ReceiptOfMyShopSerializer
+from .serializers import AddProductToCartSerializer, ReceiptOfMyShopSerializer
 
 
 # Create your views here.
 
 
-SANDBOX = ''
-#? sandbox merchant
-if SANDBOX:
-    sandbox = 'sandbox'
-else:
-    sandbox = 'www'
 
-MERCHANT = ''
-ZP_API_REQUEST = f"https://{sandbox}.zarinpal.com/pg/rest/WebGate/PaymentRequest.json"
-ZP_API_VERIFY = f"https://{sandbox}.zarinpal.com/pg/rest/WebGate/PaymentVerification.json"
-ZP_API_STARTPAY = f"https://{sandbox}.zarinpal.com/pg/StartPay/"
-amount = 1000  # Rial / Required
-description = "نهایی کردن خرید از سایت ما"  # Required
-phone = ''  # Optional
-# Important: need to edit for realy server.
-CallbackURL = 'http://127.0.0.1:8000/cart/verify/'
 
 
 class AddProductToCartView(APIView):
     class_serializer = AddProductToCartSerializer
-    throttle_classes = [AnonRateThrottle,UserRateThrottle]
-    permission_classes = [IsAuthenticatedOrReadOnly,]
+    throttle_classes = [AnonRateThrottle, UserRateThrottle]
+    permission_classes = [IsAuthenticatedOrReadOnly, ]
     """
     Cart_shop request is post 
     
     information: id product send to me with post.request!, and user this api for get request
     """
-    def post(self,request: HttpRequest):
+
+    def post(self, request: HttpRequest):
         ser_data = AddProductToCartSerializer(data=request.POST)
-        if request.user.is_authenticated:
-            if ser_data.is_valid():
-                count_of_product = ser_data.validated_data.get('count')
-                if count_of_product < 1:
-                    return Response(data={'message': 'در دادن نوع درخواست خود دقت کنید'},status=status.HTTP_409_CONFLICT)
-                else:
-                    product_id = ser_data.validated_data.get('product_id')
-                    product = Product.objects.filter(is_delete=False,is_active=True,id=product_id).first()
-                    print(product)
-                    if product is not None:
-                        current_cart, created = Cart.objects.get_or_create(is_paid=False,user_id=request.user.id)
-                        current_cat_detail = current_cart.cartdetail_set.filter(product_id=product_id).first()
-                        if current_cat_detail is not None:
-                            current_cat_detail.count += int(count_of_product)
-                            current_cat_detail.save()
-                            return Response(data={'message': 'محصول به سبد خرید شما اضافه شد!'},status=status.HTTP_208_ALREADY_REPORTED)
-                        else:
-                            new_detail = CartDetail(cart_shop_id=current_cart.id,product_id=product_id,count=count_of_product)
-                            new_detail.save()
-                            return Response(data={'message': 'سبد خرید شما ایجاد شد'},status=status.HTTP_202_ACCEPTED)
-                    return Response({'message': 'درخواست شما با موفقیت ثبت شده است'},status=status.HTTP_200_OK)
-            return Response({'message': 'تداخل در نا کار آمدی درخواست'},status=status.HTTP_408_REQUEST_TIMEOUT)
-        return Response({'message': 'برای ثبت سفارش نیاز به ثبت نام است'}, status=status.HTTP_401_UNAUTHORIZED)
+        if not request.user.is_authenticated:
+            return Response({'message': 'برای ثبت سفارش نیاز به ثبت نام است'}, status=status.HTTP_401_UNAUTHORIZED)
+
+        if not ser_data.is_valid():
+            return Response({'message': 'تداخل در نا کار آمدی درخواست'}, status=status.HTTP_408_REQUEST_TIMEOUT)
+        count_of_product = ser_data.validated_data.get('count')
+
+        if count_of_product < 1:
+            return Response(data={'message': 'در دادن نوع درخواست خود دقت کنید'}, status=status.HTTP_409_CONFLICT)
+
+        product_id = ser_data.validated_data.get('product_id')
+        product = Product.objects.filter(is_delete=False, is_active=True, id=product_id).first()
+        print(product)
+        if product is None:
+            return Response({'message': 'درخواست شما با موفقیت ثبت شده است'}, status=status.HTTP_200_OK)
+
+        current_cart, created = Cart.objects.get_or_create(is_paid=False, user_id=request.user.id)
+        current_cat_detail = current_cart.cartdetail_set.filter(product_id=product_id).first()
+        if current_cat_detail is not None:
+            current_cat_detail.count += int(count_of_product)
+            current_cat_detail.save()
+            return Response(data={'message': 'محصول به سبد خرید شما اضافه شد!'},
+                            status=status.HTTP_208_ALREADY_REPORTED)
+        else:
+            new_detail = CartDetail(cart_shop_id=current_cart.id, product_id=product_id, count=count_of_product)
+            new_detail.save()
+            return Response(data={'message': 'سبد خرید شما ایجاد شد'}, status=status.HTTP_202_ACCEPTED)
+
 
 class ReceiptOfMyShopView(APIView):
 
@@ -76,19 +77,18 @@ class ReceiptOfMyShopView(APIView):
         self.query = Cart.objects.filter(is_paid=True, user_id=request.user.id).all()
         super().setup(request, *args, **kwargs)
 
-    def get(self,request: HttpRequest):
-            # e = Cart.objects.filter(id=4, is_paid=True).first()
-            # print(e)
-            ser_data = ReceiptOfMyShopSerializer(instance=self.query,many=True)
-            return Response(data=ser_data.data,status=status.HTTP_202_ACCEPTED)
-
+    def get(self, request: HttpRequest):
+        # e = Cart.objects.filter(id=4, is_paid=True).first()
+        # print(e)
+        ser_data = ReceiptOfMyShopSerializer(instance=self.query, many=True)
+        return Response(data=ser_data.data, status=status.HTTP_202_ACCEPTED)
 
 
 @login_required
 def send_request(request: HttpRequest):
     current_cart, created = Cart.objects.get_or_create(is_paid=False, user_id=request.user.id)
     total_price = current_cart.calculate_price_whole()
-    if total_price == 0:
+    if total_price <= 0:
         return redirect('http://127.0.0.1:8000/')
     data = {
         "MerchantID": MERCHANT,
@@ -103,21 +103,24 @@ def send_request(request: HttpRequest):
     try:
         response = requests.post(ZP_API_REQUEST, data=data, headers=headers, timeout=10)
 
-        if response.status_code == 200:
-            response = response.json()
-            if response['Status'] == 100:
-                return {'status': True, 'url': ZP_API_STARTPAY + str(response['Authority']),'authority': response['Authority']}
-            else:
-                return {'status': False, 'code': str(response['Status'])}
-        return response
+        if response.status_code != 200:
+            return response
+
+        response = response.json()
+        if response['Status'] == 100:
+            return {'status': True, 'url': ZP_API_STARTPAY + str(response['Authority']),
+                    'authority': response['Authority']}
+        else:
+            return {'status': False, 'code': str(response['Status'])}
 
     except requests.exceptions.Timeout:
         return {'status': False, 'code': 'timeout'}
     except requests.exceptions.ConnectionError:
         return {'status': False, 'code': 'connection error'}
 
+
 @login_required()
-def verify(authority,request: HttpRequest):
+def verify(authority, request: HttpRequest):
     print(request.user)
     print(request.user.id)
     current_cart, created = Cart.objects.get_or_create(is_paid=False, user_id=request.user.id)
@@ -132,13 +135,14 @@ def verify(authority,request: HttpRequest):
     headers = {'content-type': 'application/json', 'content-length': str(len(data))}
     response = requests.post(ZP_API_VERIFY, data=data, headers=headers)
 
-    if response.status_code == 200:
-        response = response.json()
-        if response['Status'] == 100:
-            current_cart.is_paid = True
-            current_cart.payment_date = time.time()
-            current_cart.save()
-            return {'status': True, 'RefID': response['RefID']}
-        else:
-            return {'status': False, 'code': str(response['Status'])}
-    return response
+    if response.status_code != 200:
+        return response
+    response = response.json()
+    if response['Status'] == 100:
+        current_cart.is_paid = True
+        current_cart.payment_date = time.time()
+        current_cart.save()
+        return {'status': True, 'RefID': response['RefID']}
+    else:
+        return {'status': False, 'code': str(response['Status'])}
+
