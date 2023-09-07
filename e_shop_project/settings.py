e2e596c9507c07b8861233c6cefa8a9a9cdf8abd
Hooman.sp <1380hz1380hz@gmail.com>
2023-09-07 23:58:32 +0330

    refactor docker compose

--- e_shop_project/settings.py
+++ e_shop_project/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('DJ_SECRET_KEY',get_random_secret_key())
+SECRET_KEY = os.environ.get('DJ_SECRET_KEY')
 'django-insecure-_!ef+fs_esvh64&&ul817030iqh0d)6ox#84f&mp!mcqy)5qxk'
 
 # SECURITY WARNING: don't run with debug turned on in production!
